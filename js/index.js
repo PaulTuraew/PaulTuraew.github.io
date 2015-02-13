acc4609cf7b1a73370c73cc6796f94154e6c1248
paulturaew <paul.turaew@gmail.com>
2015-02-13 13:26:28 -0500

    fade jQ feature added

--- js/index.js
+++ js/index.js
@@ -14,3 +14,26 @@ function scrollNav() {
   $('.scrollTop a').scrollTop();
 }
 scrollNav();
+
+// text fade in effects
+
+$(document).ready(function() {
+
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+
+        /* Check the location of each desired element */
+        $('#abouttext, #portfoliodiv > p').each( function(i){
+
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+
+                $(this).animate({'opacity':'1'},1000);
+
+            }
+        });
+    });
+});
